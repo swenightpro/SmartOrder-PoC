f1c41932b2e5302fe77f2f0b79dcfeb15a436c62
samuele-perozzo <samueleperozzo1@gmail.com>
2026-02-14 17:07:27 +0100

    Aggiornamento per DB postgres

--- app/api/clients/search/route.ts
+++ app/api/clients/search/route.ts
@@ -8,16 +8,17 @@ export async function GET(request: Request) {
   if (!q || q.length === 0) return NextResponse.json([]);
 
   try {
-    const [rows] = await db.execute(
-      `SELECT cod_cli, rag_soc 
-       FROM anacli 
-       WHERE rag_soc LIKE ? OR cod_cli = ? 
-       LIMIT 10`,
-      [`%${q}%`, q]
-    );
-    
-    return NextResponse.json(rows);
+  const [rows] = await db.execute(
+    `SELECT cod_cli, rag_soc 
+     FROM anacli 
+     WHERE rag_soc ILIKE ? OR CAST(cod_cli AS TEXT) = ? 
+     LIMIT 10`,
+    [`%${q}%`, q]
+  );
+  
+  return NextResponse.json(rows);
   } catch (error: any) {
+    console.error("Errore ricerca clienti:", error); // Aggiungi questo per vedere l'errore nei log
     return NextResponse.json({ error: 'Errore ricerca clienti' }, { status: 500 });
   }
 }
